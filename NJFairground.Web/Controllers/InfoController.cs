afc4efcc27e65ac034b820e031de72ce8317f112
rupeshjha2000 <rupeshjha2000@gmail.com>
2014-05-27 00:23:11 +0530

    Styles and Scripts added

---
+++ NJFairground.Web/Controllers/InfoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NJFairground.Web.Controllers
+{
+    public class InfoController : Controller
+    {
+        //
+        // GET: /Info/
+
+        public ActionResult Index()
+        {
+            List<string> NavItems=new List<string>();
+            NavItems.Add("The Fair");
+            NavItems.Add("Fun");
+            NavItems.Add("Info");
+            NavItems.Add("Social");
+            NavItems.Add("Map");
+
+            ViewBag.NavBarItems = NavItems;
+
+            return View();
+        }
+
+    }
+}
