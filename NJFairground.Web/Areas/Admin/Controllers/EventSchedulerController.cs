a50f9b5ed8dac353c0a059c3a27d9dd342d1f36a
Sandip Ghosh <sandipghosh.dev@gmail.com>
2014-07-02 01:52:23 +0530

    Adding controllers for the admin pannel

---
+++ NJFairground.Web/Areas/Admin/Controllers/EventSchedulerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NJFairground.Web.Areas.Admin.Controllers
+{
+    public class EventSchedulerController : Controller
+    {
+        //
+        // GET: /Admin/EventScheduler/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NJFairground.Web/Areas/Admin/Controllers/MapCreaterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NJFairground.Web.Areas.Admin.Controllers
+{
+    public class MapCreaterController : Controller
+    {
+        //
+        // GET: /Admin/MapCreater/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
