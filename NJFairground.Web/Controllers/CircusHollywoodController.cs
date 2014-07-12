550a2a48f57910ff75acdb9552f08648e77fda51
rupeshjha2000 <rupeshjha2000@gmail.com>
2014-07-13 01:55:24 +0530

    ChangeSet2:13th July

---
+++ NJFairground.Web/Controllers/CircusHollywoodController.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace NJFairground.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using NJFairground.Web.Controllers.Base;
+    using NJFairground.Web.Models;
+
+    public class CircusHollywoodController : BaseController
+    {
+        //
+        // GET: /CircusHollywood/
+        // <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        [AcceptVerbs(HttpVerbs.Get),
+        OutputCache(NoStore = true, Duration = 0, VaryByHeader = "*")]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Details", new { PageId = Convert.ToInt32(Page.CircusHollywood), PageItemId = 2201 });
+        }
+
+    }
+}
