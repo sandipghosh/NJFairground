128826c3b7c15834e996166acd08e7207ca8f372
Sandip Ghosh <sandipghosh.dev@gmail.com>
2015-04-07 02:34:31 +0530

    Minor

--- NJFairground.Web/Models/PageItemModel.cs
+++ NJFairground.Web/Models/PageItemModel.cs
@@ -2,6 +2,9 @@
 namespace NJFairground.Web.Models
 {
     using NJFairground.Web.Models.Base;
+    using NJFairground.Web.Utilities;
+    using System.Web;
+    using System.Web.Mvc;
 
     public class PageItemModel : BaseModel
     {
@@ -10,6 +13,14 @@ namespace NJFairground.Web.Models
         public string PageHeaderText { get; set; }
         public string PageSubHeaderText { get; set; }
         public string PageItemImage { get; set; }
+        public string PageItemImageUrl
+        {
+            get
+            {
+                return CommonUtility.ResolveServerUrl(string.Format("{0}{1}",
+                    CommonUtility.GetAppSetting<string>("UploadFolderItemImagePath"), this.PageItemImage), false);
+            }
+        }
         public string PageItemDetailText { get; set; }
         public string PageItemSubDetail { get; set; }
         public int StatusId { get; set; }
--- NJFairground.Web/Utilities/CommonUtility.cs
+++ NJFairground.Web/Utilities/CommonUtility.cs
@@ -700,6 +700,18 @@ namespace NJFairground.Web.Utilities
             return date.AddDays(extraDays);
         }
 
+        public static string ResolveServerUrl(string serverUrl, bool forceHttps)
+        {
+            if (serverUrl.IndexOf("://") > -1)
+                return serverUrl;
+
+            string newUrl = VirtualPathUtility.ToAbsolute(serverUrl);
+            Uri originalUri = System.Web.HttpContext.Current.Request.Url;
+            newUrl = (forceHttps ? "https" : originalUri.Scheme) +
+                "://" + originalUri.Authority + newUrl;
+            return newUrl;
+        } 
+
         #region Private Members
         /// <summary>
         /// Logs to file.
