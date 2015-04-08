b2bde27449b81c1e0e21b8f297ef21a38dc77833
Sandip Ghosh <sandipghosh.dev@gmail.com>
2015-04-08 23:55:13 +0530

    Changeset - 1 on 08/04/2015

---
+++ NJFairground.Web/Filters/ApiAuthenticationFilterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace NJFairground.Web.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,
+        AllowMultiple = false, Inherited = false)]
+    public class ApiAuthenticationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
